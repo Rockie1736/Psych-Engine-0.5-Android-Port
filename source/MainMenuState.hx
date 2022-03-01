a5105c524b99e5326efd6f2a74946458a92b70b4
Rockie1736 <100703399+Rockie1736@users.noreply.github.com>
2022-03-01 11:34:50 -0500

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -114,7 +114,8 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			menuItem.screenCenter(X);
+			//menuItem.screenCenter(X);
+			menuItem.x += 200;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
@@ -270,7 +271,7 @@ class MainMenuState extends MusicBeatState
 
 		menuItems.forEach(function(spr:FlxSprite)
 		{
-			spr.screenCenter(X);
+			//spr.screenCenter(X);
 		});
 	}
 
